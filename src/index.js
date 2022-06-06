70c0dd655c8d2115bf5a7664e21a8c872a42a2b4
Nikola M <nikola.merlic2@gmail.com>
2022-06-06 21:44:03 +0200

    Podaci hard codani u store.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import data from './store';
+
 
 const app = express()
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/",(req, res)=>{
-    res.send("Hello World!");
+    res.send("Hello!");
 });
 
 app.listen(port, ()=>{
     console.log(`Example app listening on port${port}`)
 })
+
+
+app.get('/ljubimci', (req, res)=> res.json(data.ljubimci));
+
